927aa2f73026a253adc7497172e0068dce2f43b7
heavypete <70510263+heavypete@users.noreply.github.com>
2022-03-04 12:33:28 +0100

    images load... bad layout

--- src/App.js
+++ src/App.js
@@ -1,9 +1,77 @@
+import { useEffect, useState } from "react";
+import { Spacer, Box } from "@chakra-ui/react";
+import { Flex } from "@chakra-ui/react";
+import { Button, ButtonGroup } from "@chakra-ui/react";
 import "./App.scss";
 
+const url = "https://api.thedogapi.com/v1/images/search?limit=6";
+
 function App() {
+  const [dogs, setDogs] = useState([]);
+  const [page, setPage] = useState("main");
+  const [detailsDog, setDetailsDog] = useState(null);
+
+  // data fetch
+  useEffect(() => {
+    (async () => {
+      const response = await fetch(url);
+      const data = await response.json();
+      setDogs([...data]);
+    })();
+  }, []);
+
+  // simulation of 2 pages
+
+  const togglePage = (dog = null) => {
+    const _page = page === "main" ? "details" : "main";
+    setDetailsDog({ ...dog });
+    setPage(_page);
+    console.log(page);
+  };
+
+  const getBreedName = (dog) => {
+    if (dog.breeds.length === 0) {
+      return "(no breed info)";
+    } else {
+      return dog.breeds[0].name;
+    }
+  };
+
   return (
     <div className="App">
-      <p>Howdy</p>
+      <h1> All the Dogs</h1>
+      {dogs.length > 0 && <p> There are {dogs.length} dogs.</p>}
+      {/* MAIN PAGE */}
+      {page === "main" && (
+        <Flex className="container" flexWrap="wrap">
+          {dogs.map((dog, index) => {
+            return (
+              <Box key={index} w="100%" p={6} color="white" className="dog">
+                <h3>{getBreedName(dog)}</h3>
+                <img src={dog.url} onClick={() => togglePage(dog)} alt="dog" />
+              </Box>
+            );
+          })}
+        </Flex>
+      )}
+      {/* BREED DETAIL PAGE */}
+      {page === "details" && (
+        <div>
+          <Button onClick={togglePage} className="returnButton">
+            Go Back
+          </Button>
+          {detailsDog.breeds.length >
+            0(
+              <div>
+                <h2>{detailsDog.breeds[0].name}</h2>
+                <div className="temperament">
+                  {detailsDog.breeds[0].temperament}
+                </div>
+              </div>
+            )}
+          <img src={detailsDog.url} alt="dog" />
+        </div>
+      )}
     </div>
   );
 }
